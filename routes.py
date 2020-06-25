4e4ec4c5ffc3c47f615cabfe45dcbcb5133eb25f
tushar2407 <tushar.mohan2001@gmail.com>
2020-06-26 03:26:38 +0530

    models done

--- routes.py
+++ routes.py
@@ -1,14 +1,20 @@
 from app import app,db
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, flash, get_flashed_messages
 import forms
+from models import Task
+from datetime import datetime
 @app.route('/')
 @app.route('/index')
-def home():
-    return render_template('index.html')
-@app.route('/about', methods=['GET','POST'])
-def about():
+def index():
+    tasks=Task.query.all()
+    return render_template('index.html',tasks=tasks)
+@app.route('/add', methods=['GET','POST'])
+def add():
     form=forms.AddTaskForm()
     if form.validate_on_submit():
-        print(form.title.data)
-        return render_template('about.html',form=form,title=form.title.data)
-    return render_template('about.html',form=form)
+        #print(form.title.data)
+        t=Task(title=form.title.data, date=datetime.utcnow())
+        db.session.add(t)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html',form=form)
